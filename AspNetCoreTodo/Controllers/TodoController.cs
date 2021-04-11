770c311be0007de7c3448ab79cb56c7e110dcdc4
Peter <dhondt.peter@gmail.com>
2021-04-11 11:49:09 +0200

    configured dependency injection

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index()
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        
+        public async Task<IActionResult> Index()
         {
             // Get to-do items from database
+            var items = await _todoItemService.GetIncompleteItemsAsync();
 
             // Put items into a model
 
