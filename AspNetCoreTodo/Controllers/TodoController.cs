bc4b8945f248a8cc6a51962ef9f7901a0a5eccf3
Peter <dhondt.peter@gmail.com>
2021-04-11 12:28:17 +0200

    finished the controller

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreTodo.Services;
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Controllers
 {
@@ -22,8 +23,13 @@ namespace AspNetCoreTodo.Controllers
             var items = await _todoItemService.GetIncompleteItemsAsync();
 
             // Put items into a model
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
 
             // Render view using the model
+            return View(model);
         }
     }
 }
