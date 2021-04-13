c8b949a0870b4fc658ac4b41c45a78400b386078
Peter <dhondt.peter@gmail.com>
2021-04-13 10:33:07 +0200

    created functionality to add new todo items

--- AspNetCoreTodo/Services/TodoItemService.cs
+++ AspNetCoreTodo/Services/TodoItemService.cs
@@ -24,5 +24,17 @@ namespace AspNetCoreTodo.Services
                 .ToArrayAsync();
             return items;
         }
+        
+        public async Task<bool> AddItemAsync(TodoItem newItem)
+        {
+            newItem.Id = Guid.NewGuid();
+            newItem.IsDone = false;
+            newItem.DueAt = DateTimeOffset.Now.AddDays(3);
+
+            _context.Items.Add(newItem);
+
+            var saveResult = await _context.SaveChangesAsync();
+            return saveResult == 1;
+        }
     }
 }
